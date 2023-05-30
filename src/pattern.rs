aa8793a4d5242e042f47fd7ee102fbbe443e9aee
Andrew Zdybak <azdybak@olsonresearchpa.com>
2023-05-29 23:32:32 -0400

    chipping away at patterns

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,16 +1,23 @@
 #![allow(dead_code)]
 use crate::color::Color;
+use crate::matrix::Matrix;
 use crate::raytuple::RayTuple;
+use crate::shape::Shape;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Pattern {
     pub a: Color,
     pub b: Color,
+    pub transform: Matrix,
 }
 
 impl Pattern {
     pub fn stripe_pattern(a: Color, b: Color) -> Self {
-        Self { a, b }
+        Self {
+            a,
+            b,
+            transform: Matrix::identity(),
+        }
     }
 
     pub fn stripe_at(&self, point: RayTuple) -> Color {
@@ -20,12 +27,21 @@ impl Pattern {
             self.b
         }
     }
+
+    pub fn stripe_at_object(&self, object: Shape, world_point: RayTuple) -> Color {
+        let object_point = object.transform.inverse().unwrap() * world_point;
+        let pattern_point = self.transform.inverse().unwrap() * object_point;
+
+        self.stripe_at(pattern_point)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::color::Color;
+    use crate::matrix::Matrix;
+    use crate::shape::Shape;
 
     #[test]
     fn create_striped_pattern() {
@@ -71,5 +87,45 @@ mod tests {
         assert_eq!(p.stripe_at(RayTuple::point(-0.1, 0.0, 0.0)), black);
         assert_eq!(p.stripe_at(RayTuple::point(-1.0, 0.0, 0.0)), black);
         assert_eq!(p.stripe_at(RayTuple::point(-1.1, 0.0, 0.0)), white);
+    }
+
+    #[test]
+    fn stripes_with_object_transform() {
+        let mut object = Shape::sphere();
+        object.transform = Matrix::scaling(2.0, 2.0, 2.0);
+
+        let white = Color::new(1.0, 1.0, 1.0);
+        let black = Color::new(0.0, 0.0, 0.0);
+
+        let p = Pattern::stripe_pattern(white, black);
+        let c = p.stripe_at_object(object, RayTuple::point(1.5, 0.0, 0.0));
+        assert_eq!(c, white);
+    }
+
+    #[test]
+    fn stripes_with_pattern_transform() {
+        let object = Shape::sphere();
+
+        let white = Color::new(1.0, 1.0, 1.0);
+        let black = Color::new(0.0, 0.0, 0.0);
+
+        let mut p = Pattern::stripe_pattern(white, black);
+        p.transform = Matrix::scaling(2.0, 2.0, 2.0);
+        let c = p.stripe_at_object(object, RayTuple::point(1.5, 0.0, 0.0));
+        assert_eq!(c, white);
+    }
+
+    #[test]
+    fn stripes_with_both_transform() {
+        let mut object = Shape::sphere();
+        object.transform = Matrix::scaling(2.0, 2.0, 2.0);
+
+        let white = Color::new(1.0, 1.0, 1.0);
+        let black = Color::new(0.0, 0.0, 0.0);
+
+        let mut p = Pattern::stripe_pattern(white, black);
+        p.transform = Matrix::translation(0.5, 0.0, 0.0);
+        let c = p.stripe_at_object(object, RayTuple::point(2.5, 0.0, 0.0));
+        assert_eq!(c, white);
     }
 }
